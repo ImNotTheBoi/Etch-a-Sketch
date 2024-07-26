cdca28ecbbfaa1b1d9c3163bbfc41c11d90745a0
ImNotTheBoi <174925899+ImNotTheBoi@users.noreply.github.com>
2024-07-26 21:57:54 +0800

    Website UI
    Finish website ui which were separated into 2 sections which have either a header or footer.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,8 @@
+// Variables
 const gridContainer = document.querySelector("#gridContainer")
-StyleSheet
-function createDiv (num) {
-    for (let i = 0; i < num; ++i) {
-        const newDiv = document.createElement("div")
-        gridContainer.appendChild(newDiv)
-    }
-}
+const createGrid = document.querySelector("#createGrid")
 
+// Starter Grid
 createDiv(1024);
 let divArray = document.querySelectorAll("#gridContainer div")
 
@@ -14,6 +10,38 @@ divArray.forEach(function(element) {
     element.setAttribute("style", "flex-basis: 3.125%")
     element.addEventListener("mouseover", function() {
         element.classList.add("hover")
-        console.log(element)
     })
+})
+
+function createDiv (num) {
+    for (let i = 0; i < num; ++i) {
+        const newDiv = document.createElement("div")
+        gridContainer.appendChild(newDiv)
+    }
+}
+
+// Change Grid Button
+createGrid.addEventListener("click", function() {
+    let promptInput = parseInt(prompt("Input your preferred grid size(Maximum grid size is 100).", "0"), 10)
+    if (typeof promptInput === "number" && Number.isInteger(promptInput) && promptInput > 0 && promptInput <= 100) {
+        
+        // Remove Function
+        divArray.forEach(function(element) {
+            gridContainer.removeChild(element)
+        })
+
+        //Create Div
+        let divNumber = promptInput * promptInput
+        let basisNumber = 100 / promptInput
+        createDiv(divNumber)
+
+        //Set Div
+        divArray = document.querySelectorAll("#gridContainer div")
+        divArray.forEach(function(element) {
+            element.setAttribute("style", `flex-basis: ${basisNumber}%`)
+            element.addEventListener("mouseover", function() {
+                element.classList.add("hover")
+            })
+        })
+    }
 })
