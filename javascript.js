11150b457298fff97ea797d7fa3e005d3881640c
ImNotTheBoi <174925899+ImNotTheBoi@users.noreply.github.com>
2024-07-26 18:48:40 +0800

    Editable Grid Size
    Make the grid be able to change size(100x100) using flex-basis = 100 / grid size.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const gridContainer = document.querySelector("#gridContainer")
-
+StyleSheet
 function createDiv (num) {
     for (let i = 0; i < num; ++i) {
         const newDiv = document.createElement("div")
@@ -7,11 +7,11 @@ function createDiv (num) {
     }
 }
 
-createDiv(256);
-
+createDiv(1024);
 let divArray = document.querySelectorAll("#gridContainer div")
 
 divArray.forEach(function(element) {
+    element.setAttribute("style", "flex-basis: 3.125%")
     element.addEventListener("mouseover", function() {
         element.classList.add("hover")
         console.log(element)
